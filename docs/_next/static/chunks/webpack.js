0c74dd1c8fd64d1488dcb14e0fc58dcce6a17e7d
MichaelQQ <mr.tu@michaelqq.com>
2020-11-12 20:16:12 +0800

    export new version

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f574f57a09dd859bb3d5";
+/******/ 	var hotCurrentHash = "4f242d3d1fa5aebd9ee2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
