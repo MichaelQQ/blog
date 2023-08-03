45d2578fdb9f43d86dcb99403a6df28425d5122a
MichaelQQ <mr.tu@michaelqq.com>
2023-08-03 11:40:52 +0800

    Add html lang and add display swap to google font

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Pangolin"
+          href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Pangolin&display=swap"
           rel="stylesheet"
         />
         <link
